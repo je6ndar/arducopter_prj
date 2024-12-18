73e0bea03fbc96a73deb84edc92a8d4910189302
je6ndar <f.vasyliev@gmail.com>
2024-12-18 08:40:52 +0000

    pid + msp

---
+++ PID_config.py
@@ -0,0 +1,23 @@
+#Roll PID
+roll_P = 1
+roll_I = 1
+roll_D = 1
+roll_previous_error = 0
+
+#Pitch PID
+pitch_P = 1
+pitch_I = 1
+pitch_D = 1
+pitch_previous_error = 0
+
+#Yaw PID
+yaw_P = 1
+yaw_I = 1
+yaw_D = 1
+yaw_previous_error = 0
+
+#Throttle PID
+throttle_P = 1
+throttle_I = 1
+throttle_D = 1
+throttle_previous_error = 0
--- radioEmulation.py
+++ radioEmulation.py
@@ -1,10 +1,6 @@
-#Roll PID
+from PID_config import *
 
-#Pitch PID
-
-#Yaw PID
-
-#Throttle PID
+dt = 1/100          #100Hz loop
 
 '''
 Get PID coeffs?
@@ -13,5 +9,34 @@ MSP_PID  112    //out message    P I D coeff (9 are used currently)
 '''
 
 '''
-Raw PID -> clamp Raw PID -> 
+Raw PID -> clamp Raw PID -> map it onto the pwm value 
 '''
+
+
+#returns pid, previous error based on the given params 
+def get_pid(current_state, desired_state, previous_error, P, I, D):
+    current_error = desired_state - current_state
+    dErr = current_error - previous_error
+    p_term = P * current_error
+    i_term = I * dErr * dt                      #simple trapezoid rule
+    d_term = D * dErr / dt
+    pid = p_term + i_term + d_term
+    return pid, current_error                   #current error becomes previous
+
+
+def get_throttle_pid():
+    pass
+
+def get_yaw_pid():
+    pass
+
+def get_pitch_pid():
+    pass
+
+def get_roll_pid():
+    pass
+
+
+
+
+
